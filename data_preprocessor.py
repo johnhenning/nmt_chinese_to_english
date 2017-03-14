1d35e651ec3677c962d0161aa12fa1b3c2e6e031
John Henning <jlhbaseball15@me.com>
2017-03-13 22:40:49 -0400

    Finished Dataset Creation. Need to add saving

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -4,6 +4,13 @@ import h5py
 import os
 import xml.etree.ElementTree as ET
 
+
+class CorpusFileMapping:
+    def __init__(self,english_filename,chinese_filename,sentence_mappings):
+        self.english_filename = english_filename
+        self.chinese_filename = chinese_filename
+        self.sentence_mappings = sentence_mappings
+
 class Sentence:
     def __init__(self, sentence, tag):
         self.tag = tag
@@ -17,7 +24,9 @@ class DatasetProcessor:
         self.ChineseDataset = []
 
     def CreateDataset(self, filename, saveDictionary=True, saveDataset=True):
-        english_corpus_files, chinese_corpus_files = self.LoadCorpusFiles(filename)
+        sentence_mappings = self.ReadSentenceMapping(filename)
+
+        self.ProcessSentenceMappings(sentence_mappings)
 
         for f in english_corpus_files:
             self.EnglishDataset.extend(ProcessCorpusFile(f)) #returns tokenized sentences
@@ -40,22 +49,70 @@ class DatasetProcessor:
         for f in files:
             f.close()
 
+    def ProcessSentenceMappings(self,file_mappings):
+        for fm in file_mappings:
+            english_data = self.ProcessCorpusFile(fm.english_file,'English')
+            chinese_data = self.ProcessCorpusFile(fm.chinese_file, 'Chinese')
+            english_data, chinese_data = self.AlignDatasets(english_data,chinese_data,fm.sentence_mappings)
+            self.EnglishDataset.extend(english_data)
+            self.ChineseDataset.extend(chinese_data)
+
+    def ReadSentenceMapping(self,xml_file):
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+        file_maps = []
+        for linkGroup in root:
+            english_file = linkGroup.attrib['fromDoc']
+            chinese_file = linkGroup.attrib['toDoc']
+            sentence_mappings = []
+            for link in linkGroup:
+                mapping = processXMLMapping(link.attrib['xtargets'])
+                sentence_mappings.append(mapping)
+
+            file_map = CorpusFileMapping(english_file,chinese_file,sentence_mappings)
+            file_maps.append(file_map)
+
+        return file_maps
+
+    def AlignDatasets(self,english_data,chinese_data,sentence_mappings):
+        edata = []
+        cdata = []
+        for sm in sentence_mappings:
+            english = []
+            for i in sm[0]:
+                english.extend(english_data[i])
+            chinese = []
+            for i in sm[1]:
+                chinese.extend(chinese_data[i])
+
+            edata.append(english)
+            cdata.append(chinese)
+
+        return edata, cdata
+
+
+    def processXMLMapping(self,link_attrib):
+        english_chinese_split = link_attrib.split(';')
+        english_chinese_split[0] = map(int,english_chinese_split[0].split(' '))
+        english_chinese_split[1] = map(int,english_chinese_split[1].split(' '))
+        return english_chinese_split
+
     #this will need to change based on different xml structures, but for our data set, this splits and tokenizes the sentences
-    def ProcessCorpusFile(self, file):
+    def ProcessCorpusFile(self, filename, language):
         data =[]
-        tree = ET.parse(file)
+        tree = ET.parse(filename)
         root = tree.getroot()
 
         for child in root:
             data.append("<s>")
             for token in child:
                 if(token.tag == 'w'):
+                    self.AddToDictionary(token.text, language)
                     data.append(token.text)
             data.append("</s>")
 
         return data
 
-    def Tokenize(self):
 
 
     def FillDictionaryWithCorpus(self,language,corpus):
@@ -91,6 +148,3 @@ class DatasetProcessor:
     def saveDictionary(self):
 
     def saveDataset(self):
-
-
-
